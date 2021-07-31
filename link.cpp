ee35d559a9878e1e85a157bc560be08d17ac1553
david088 <49335420+david088@users.noreply.github.com>
2021-07-30 17:42:22 -0700

    Update link.cpp

--- link.cpp
+++ link.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 
 int main(){
-  cout << "Please visit the link in link.md folder to see all project files!\nhttps://drive.google.com/drive/folders/1o4TYK6YRGj6Ut-PryLelw7B58mKv7_dD?usp=sharing" << endl;
+  cout << "Please visit the link in link.md folder to see all project files!\n"+
+    "https://drive.google.com/drive/folders/1o4TYK6YRGj6Ut-PryLelw7B58mKv7_dD?usp=sharing" << endl;
 }
